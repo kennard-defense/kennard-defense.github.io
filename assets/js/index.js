c3db733090d286edce17f450e37b56217ba5b6e6
Neha Kennard <nnayak@cs.umass.edu>
2020-11-28 15:39:09 -0800

    first update from andrew

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,13 @@ switchTab(0); // Display the first tab
 
 function switchTab(delta) {
   var tabs = document.getElementsByClassName("tab");
-  newTab = (currentTab + delta + tabs.length) % tabs.length;
+  newTab = (currentTab - delta + tabs.length) % tabs.length;
   console.log(newTab)
   tabs[currentTab].style.display = "none";
   tabs[newTab].style.display = "block";
   currentTab = newTab;
+}
+
+function get_slide_name(base_url, slide_number) {
+  return base_url + "/assets/slides/KennardDefense." + slidenumber.padstart(3, 0) + ".png"
 }
